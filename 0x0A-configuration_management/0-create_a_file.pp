30b362016f967123112a18ce06980878c5073857
endritNovaku <ennald@outlook.com>
2022-08-15 05:55:35 -0700

    fixing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file {'school':
 ensure  => 'present',
-path => '/tmp/school',
-mode => '0744',
-group => 'www-data',
-owner => 'www-data',
-content => "I love Puppet",
+path    => '/tmp/school',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
+content => 'I love Puppet',
 }
