9a0422984cb3c9c38c439fabc91331d747ae867f
endritNovaku <ennald@outlook.com>
2022-09-13 05:54:01 -0700

    fix

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Print a user and their completed tasks"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users/{}"\
+               .format(int(sys.argv[1]))
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+               .format(int(sys.argv[1]))
+    todos = requests.get(todo_url)
+    user = requests.get(user_url)
+
+    todo = todos.json()
+    user_json = user.json()
+
+    with open("{}.csv".format(sys.argv[1]), 'w') as f:
+        spamwriter = csv.writer(f, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+        for i in range(0, len(todo)):
+            spamwriter.writerow([user_json['id'],
+                                 user_json['name'],
+                                 todo[i]['completed'],
+                                 todo[i]['title']])
